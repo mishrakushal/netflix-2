8d1d2cfac22306be79a79717db4ff9ca170e373a
Kushal Mishra <kuskalm@gmail.com>
2023-05-30 20:18:48 +0530

    Created superuser and sample database entries

--- netflixprj/netflixapp/admin.py
+++ netflixprj/netflixapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from netflixapp.models import Movie, CustomUser, Video, Profile
+
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(CustomUser)
+admin.site.register(Video)
+admin.site.register(Profile)
