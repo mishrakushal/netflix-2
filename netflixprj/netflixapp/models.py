3d86775afebdf75b259ef91b45ce850c16b73bc4
Kushal Mishra <kuskalm@gmail.com>
2023-05-30 19:07:46 +0530

    Created and migrated models for the app

--- netflixprj/netflixapp/models.py
+++ netflixprj/netflixapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+AGE_CHOICES = {
+    ('All', 'All'),
+    ('Kids', 'Kids'),
+}
+
+MOVIE_CHOICES = {
+    ('seasonal', 'Seasonal'),
+    ('single', 'Single'),
+}
+
+
+class CustomUser(AbstractUser):
+    profiles = models.ManyToManyField('Profile', blank=True)
+
+
+class Profile(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    name = models.CharField(max_length=1000)
+    age_limit = models.CharField(choices=AGE_CHOICES, max_length=10)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Movie(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4)
+    title = models.CharField(max_length=1000)
+    description = models.CharField(blank=True, null=True, max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    type = models.CharField(choices=MOVIE_CHOICES, max_length=10)
+    video = models.ManyToManyField('Video')
+    image = models.ImageField(upload_to='covers')
+    age_limit = models.CharField(choices=AGE_CHOICES, max_length=10)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=1000)
+    file = models.FileField(upload_to='movies')
+
+    def __str__(self) -> str:
+        return self.title
--- netflixprj/netflixprj/settings.py
+++ netflixprj/netflixprj/settings.py
@@ -137,3 +137,5 @@ LOGOUT_REDIRECT_URL = ''
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'netflixapp.CustomUser'
