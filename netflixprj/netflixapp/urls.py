c5932c015c448a70053fbd563ccdc45a0701d374
Kushal Mishra <kuskalm@gmail.com>
2023-06-17 19:44:18 +0530

    Added feature to create user profile

--- netflixprj/netflixapp/urls.py
+++ netflixprj/netflixapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import Home
+from .views import Home, ProfileLists, ProfileCreate
 
 app_name = 'netflixapp'
 
 urlpatterns = [
     path('', Home.as_view(), name="Home"),
+    path('profiles/', ProfileLists.as_view(), name="profile-list"),
+    path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
 ]
--- netflixprj/netflixapp/views.py
+++ netflixprj/netflixapp/views.py
@@ -1,6 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .forms import ProfileForm
+from .models import Profile
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('netflixapp:profile-list')
         return render(request, 'index.html')
+
+@method_decorator(login_required, name='dispatch')
+class ProfileLists(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+        context = {
+            'profiles': profiles
+        }
+        return render(request, 'profileList.html', context)
+
+@method_decorator(login_required, name='dispatch')
+class ProfileCreate(View):
+    def get(self, request, *args, **kwargs):
+        form = ProfileForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'profileCreate.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = ProfileForm(request.POST or None)
+        if form.is_valid():
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('netflixapp:profile-list')
+        context = {
+            'form': form
+        }
+        return render(request, 'profileCreate.html', context)
