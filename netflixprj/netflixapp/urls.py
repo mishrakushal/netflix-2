d0b5d02c4295ad7832d7cb942c8f2b5b40ab12f9
Kushal Mishra <kuskalm@gmail.com>
2023-06-18 20:20:56 +0530

    Implemented movie listing feature

--- netflixprj/netflixapp/urls.py
+++ netflixprj/netflixapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, ProfileLists, ProfileCreate
+from .views import Home, ProfileLists, ProfileCreate, MovieList
 
 app_name = 'netflixapp'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', Home.as_view(), name="Home"),
     path('profiles/', ProfileLists.as_view(), name="profile-list"),
     path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
 ]
--- netflixprj/netflixapp/views.py
+++ netflixprj/netflixapp/views.py
@@ -3,13 +3,15 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .forms import ProfileForm
-from .models import Profile
+from .models import Profile, Movie
+
 
 class Home(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('netflixapp:profile-list')
         return render(request, 'index.html')
+
 
 @method_decorator(login_required, name='dispatch')
 class ProfileLists(View):
@@ -19,6 +21,7 @@ class ProfileLists(View):
             'profiles': profiles
         }
         return render(request, 'profileList.html', context)
+
 
 @method_decorator(login_required, name='dispatch')
 class ProfileCreate(View):
@@ -40,3 +43,20 @@ class ProfileCreate(View):
             'form': form
         }
         return render(request, 'profileCreate.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class MovieList(View):
+    def get(self, request, profile_id, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(uuid=profile_id)
+            movies = Movie.objects.filter(age_limit=profile.age_limit)
+
+            if profile not in request.user.profiles.all():
+                return redirect('netflixapp:profile-list')
+            context = {
+                'movies': movies
+            }
+            return render(request, 'movieList.html', context)
+        except Profile.DoesNotExist:
+            return redirect('netflixapp:profile-list')
